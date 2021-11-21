53177ba1354f660ec669752228e16cc304e71e5a
unknown <rubio-manana@hotmail.com>
2021-11-21 15:37:32 +0100

    husarnet changes

--- src/Globals.cpp
+++ src/Globals.cpp
@@ -2,7 +2,6 @@
 /* create an instance of Preferences library to save variables*/
 Preferences preferences;
 DB_Comm DB;//create settings DB
-<<<<<<< HEAD
 ///////////////
 // Variables
 ///////////////
@@ -27,19 +26,3 @@ Inputs DI5(DI5_Pin, &DB.DigitalInput5.x);
 Inputs DI6(DI6_Pin, &DB.DigitalInput6.x);
 Inputs DI7(DI7_Pin, &DB.DigitalInput7.x);
 Inputs DI8(DI8_Pin, &DB.DigitalInput8.x);
-=======
-#ifdef XHusarnet
-    String Husarnet_read;
-    String Husarnetweb_read;
-    String CommAddress;
-    String CommValue;
-    const String GarageHTML[] = {"<!DOCTYPE html><html><head><meta name=\"viewport\" content=\"width=device-width, initial-scale=1\"><link rel=\"icon\" href=\"data:,\"><style>html { font-family: Helvetica; display: inline-block; margin: 0px auto; text-align: center;}.button { background-color: #4CAF50; border: none; color: white; padding: 16px 40px;text-decoration: none; font-size: 30px; margin: 2px; cursor: pointer;}.button2 {background-color: #555555;}</style></head><body><h1>Garagedoor</h1><p>Open the door</p>"
-                                ,"<p><a href=\"/web/O1/1\"><button class=\"button\">Open</button></a></p>"
-                                ,"<p><a href=\"/web/O1/1\"><button class=\"button button2\">Open</button></a></p>"
-                                ,"<p>Close the door</p>"
-                                ,"<p><a href=\"/web/O1/0\"><button class=\"button\">Close</button></a></p>"
-                                ,"<p><a href=\"/web/O1/0\"><button class=\"button button2\">Close</button></a></p>"
-                                ,"</body></html>"};
-    String Send_HTML;
-#endif
->>>>>>> 8057c6cc97a52b975d8769149de8a3764bd42259
